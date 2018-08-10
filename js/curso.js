423009b27f74bfb9c05765b1cf82421ff0e601e9
Angel Bulnes <angel_bulnes16@yahoo.com>
2018-08-10 17:18:17 -0600

    Validacion de formularios de seccion y curso

---
+++ js/curso.js
@@ -0,0 +1,48 @@
+var seccion = $("#slc-secciones"),
+    n_curso = $("#txt-ncurso");
+    descripcion = $("#txt-descripcion");
+
+
+/**
+ * Funcion para validar el formulario
+ */
+function validar(){
+    var listo = true;
+    if(seccion.val()=='Seleccione seccion'){
+        seccion.removeClass("is-valid");
+        seccion.addClass("is-invalid");
+        listo = false;
+    }else{
+        seccion.removeClass("is-invalid");
+        seccion.addClass("is-valid");
+    }
+
+    if(n_curso.val()==''){
+        n_curso.removeClass("is-valid");
+        n_curso.addClass("is-invalid");
+        listo = false;
+    }else{
+        n_curso.removeClass("is-invalid");
+        n_curso.addClass("is-valid");
+    }
+
+    if(descripcion.val()==''){
+        descripcion.removeClass("is-valid");
+        descripcion.addClass("is-invalid");
+        listo = false;
+    }else{
+        descripcion.removeClass("is-invalid");
+        descripcion.addClass("is-valid");
+    }
+
+    return listo;
+}
+
+$("#btn-crear").click(function(){
+    if(validar()){
+        var parametros = "seccion="+seccion.val()+
+                         "&nombre_curso="+n_curso.val()+
+                         "&descripcion="+descripcion.val();
+        console.log(parametros);   
+    }
+});
