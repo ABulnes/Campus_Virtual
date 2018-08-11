4d2d691f65c747bd8d52e005d120fa4e6e4f90f6
RobertMndz <38774856+RobertMndz@users.noreply.github.com>
2018-08-10 19:42:10 -0600

    validar matricula

---
+++ js/matricula.js
@@ -0,0 +1,47 @@
+var	facultad = $("#slc-facultad");
+	clase = $("#slc-clase");
+	seccion = $("#slc-seccion");
+	
+
+function validar(){
+	var listo = true;
+	if(facultad.val()==''){
+		facultad.removeClass("is-valid");
+		facultad.addClass("is-invalid");
+		listo=false;
+	}else{
+		facultad.removeClass("is-invalid");
+		facultad.addClass("is-valid");
+	}
+	
+	if(clase.val()==''){
+		clase.removeClass("is-valid");
+		clase.addClass("is-invalid");
+		listo=false;
+	}else{
+		clase.removeClass("is-invalid");
+		clase.addClass("is-valid");
+	}
+	
+	if(seccion.val()==''){
+		seccion.removeClass("is-valid");
+		seccion.addClass("is-invalid");
+		listo=false;
+	}else{
+		seccion.removeClass("is-invalid");
+		seccion.addClass("is-valid");
+	}
+	
+	return listo;
+}
+
+$("#btn-matricular").click(function{
+	if (validar()){
+		var parametros = "facultad="+facultad.val()+
+						 "clase="+clase.val()+
+						 "seccion"+seccion.val();
+		console.log(parametros);
+	}
+});
+	
+	
